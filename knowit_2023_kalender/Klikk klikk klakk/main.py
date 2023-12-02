d0bb8d343242ad5aab27fcb934a0dbfbfb3949c3
s473907 <113636030+s473907@users.noreply.github.com>
2023-12-02 08:55:02 +0100

    bedre time complexity på klikk

--- knowit_2023_kalender/Klikk klikk klakk/main.py
+++ knowit_2023_kalender/Klikk klikk klakk/main.py
@@ -1,26 +1,4 @@
-file_path = input("path to log.txt: ")
-
-def count_elements_in_file(file_path, delimiter=','):
-    try:
-        with open(file_path, 'r') as file:
-            file_content = file.read()
-            elements_list = file_content.split(delimiter)
-            list = []
-            for string in elements_list:
-                if string[-1] == ' ':
-                    continue
-                number = int(string[-1])
-                if "klakk" in string:
-                   list.append([True, number])
-                else:
-                   list.append([False, number])
-            return list
-        
-    except FileNotFoundError:
-        print("The specified file was not found.")
-        return []
-
-list = count_elements_in_file(file_path)
+file_path = 'knowit_2023_kalender\Klikk klikk klakk\log.txt'
 
 dict = {}
 amount = 0
@@ -29,16 +7,29 @@ def fill_list():
     for i in range(1,8):
         dict.update({i : 0})
 
-for i, sublist in enumerate(list):
-    klakk, number = sublist
-    if number in dict and not klakk:
-        dict.pop(number)
-    if number not in dict and klakk:
-        dict.update({number : 0})
-    if not dict:
-        amount += 1
-        fill_list()
-
+try:
+    with open(file_path, 'r') as file:
+        
+        file_content = file.read()
+        elements_list = file_content.split(',')
+            
+        for string in elements_list:
+            
+            if string[-1] == ' ':
+                continue
+            number = int(string[-1])
+            
+            if not dict:
+                amount += 1
+                fill_list()
+            if "klakk" in string and number not in dict:
+                dict.update({number : 0})
+            if "klikk" in string and number in dict:
+                 dict.pop(number)
+        
+except FileNotFoundError:
+    print("The specified file was not found.")
+    
 print(amount)
         
          
